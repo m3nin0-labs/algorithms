bb868cc2d971ccaccaf4e733014e837ab917cf61
Phelps <phelpsmemo@gmail.com>
2019-09-14 17:06:13 -0300

    Adição de comentário

--- src/stack/cpp/text.cpp
+++ src/stack/cpp/text.cpp
@@ -1,10 +1,13 @@
+/**
+ * Script com exemplo de utilização da classe de Pilha (Stack) de tamanho fixo (Máximo de 10 elementos)
+ */
+
 #include <iostream>
 #include "stack.h"
 
 using namespace std;
 
 int main() {
-
     StringStackArray * stringStackArray = new StringStackArray();
 
     stringStackArray->push("Tchau!");
