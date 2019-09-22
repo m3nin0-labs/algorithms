f61937c217e69a15be96531cccacb938c4ce9609
Phelps <phelpsmemo@gmail.com>
2019-09-22 17:29:44 -0300

    Implementação de uma fila com realozação de arrays

--- src/queue/cpp/queue.h
+++ src/queue/cpp/queue.h
@@ -1,6 +1,8 @@
 #ifndef QUEUE_H_INCLUDED
 #define QUEUE_H_INCLUDED
 
+#include <cstring>
+
 using namespace std;
 
 class StringQueueArray {
@@ -78,9 +80,108 @@ void StringQueueArray::checkIsEmpty() {
         throw "Queue is Empty!";
 }
 
+/**
+ * Implementação de uma fila feita através da alocação dinâmica de arrays.
+ * A ideia da implementação foi apenas criar uma forma de apresentar como a manipulação de
+ * ponteiros pode facilitar diversos processos.
+ */
 class StringQueueArrayDynamic {
+public:
+    StringQueueArrayDynamic();
+    StringQueueArrayDynamic(int size);
+    StringQueueArrayDynamic(int size, int factor);
 
+    bool isFull();
+    bool isEmpty();
+
+    string dequeue();
+    void enqueue(string item);
+
+    int getSize();
+private:
+    void upSize();
+    void checkIsEmpty();
+
+    string * itens;
+    int size, factor;
+    int startIndex = 0, endIndex = 0;
 };
+
+StringQueueArrayDynamic::StringQueueArrayDynamic() {
+    this->size = 10;
+    this->factor = 2;
+    this->itens = new string[this->size];
+}
+
+StringQueueArrayDynamic::StringQueueArrayDynamic(int size) {
+    this->size = size;
+    this->factor = 2;
+    this->itens = new string[size];
+}
+
+StringQueueArrayDynamic::StringQueueArrayDynamic(int size, int factor) {
+    this->size = size;
+    this->factor = factor;
+    this->itens = new string[size];
+}
+
+int StringQueueArrayDynamic::getSize() {
+    return this->size;
+}
+
+/**
+ * Método para mudança no tamanho do array alocado para o armazenamento dos dados
+ * na fila 
+ */
+void StringQueueArrayDynamic::upSize() {
+    // Atualiza o tamanho total do array 
+    this->size *= this->factor;
+
+    // Criando novo array
+    string * tempArray = new string[this->size];
+
+    // Copiando todos os componentes para os novos endereços da memória
+    string *pToOld = this->itens;
+    string *pToNew = tempArray;
+
+    for(int i = 0; i < (this->size / this->factor); i++) {
+        // Navega pelos endereços de memória e faz as cópias de cada posição
+        *(pToNew + i) = *(pToOld + i);
+    }
+
+    delete [] this->itens;
+    this->itens = tempArray;
+}
+
+bool StringQueueArrayDynamic::isFull() {
+    return this->size == this->endIndex;
+}
+
+bool StringQueueArrayDynamic::isEmpty() {
+    return this->endIndex == 0;
+}
+
+void StringQueueArrayDynamic::checkIsEmpty() {
+    if (this->isEmpty())
+        throw "Queue is Empty!";
+}
+
+void StringQueueArrayDynamic::enqueue(string item) {
+    if (this->isFull()) {
+        // Caso esteja cheio, é feito uma atualização
+        // no tamanho do array
+        this->upSize();
+    } 
+    this->itens[this->endIndex] = item;
+    (this->endIndex)++;
+}
+
+string StringQueueArrayDynamic::dequeue() {
+    this->checkIsEmpty();
+
+    (this->startIndex)++;
+    return this->itens[this->startIndex--];
+}
 
 class StringQueueLinkedList {
 
